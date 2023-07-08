d6e79c588e2667313156ff7125fbce625a379d4a
Zach Antunes <ztunes4@gmail.com>
2023-07-08 13:02:39 -0700

    Final product. Added more comments, finished writing the function(s) that generate the license badge.

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,46 +1,80 @@
+// array that sets up the license badge
+
+const licenseArray = [
+  {
+    name: 'License: Apache',
+    link: 'https://opensource.org/licenses/Apache-2.0',
+    shield: 'https://img.shields.io/badge/License-Apache_2.0-blue.svg', 
+  },
+  {
+    name: 'License: GNU',
+    link: 'https://www.gnu.org/licenses/gpl-3.0',
+    shield: 'https://img.shields.io/badge/License-GNU-blue.svg', 
+  },
+  {
+    name: 'License: MIT',
+    link: 'https://opensource.org/licenses/MIT',
+    shield: 'https://img.shields.io/badge/License-MIT-yellow.svg', 
+  },
+
+]
+
 // TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {
-  return``
-}
+// function that generates the license badge
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-  return `
-  ${renderLicenseLink()}
-  `
+function renderLicenseBadge(license) {
+ if (license === 'Apache')
+ 
+ return `[![${licenseArray[0].name}](${licenseArray[0].shield})](${licenseArray[0].link})`
+
+ else if (license === 'GNU')
+
+ return `[![${licenseArray[1].name}](${licenseArray[1].shield})](${licenseArray[1].link})`
+
+ else if (license === 'MIT')
+
+ return `[![${licenseArray[2].name}](${licenseArray[2].shield})](${licenseArray[2].link})`
+ 
 }
 
 // TODO: Create a function to generate markdown for README
+
+// function that generates the markdown for the read me
+
 function generateMarkdown(data) {
+
+  // function above passes the input from node.js into the variable below
+
+  const licenseBadge = renderLicenseBadge(data.license);
+
   return `# ${data.title}
-${renderLicenseSection(data.license)}
-## Description
-${data.description}
-## Installation
-${data.installation}
-## Usage
-${data.usage}
-## License
-${data.license}
-## Contributions
-${data.contributions}
-## Testing
-${data.testing}
-## Questions
 
-Email: ${data.email}
+  ${licenseBadge}
 
-GitHub: github.com/${data.github}
-`;
-}
+  ## Description
+  ${data.description}
+  ## Installation
+  ${data.installation}
+  ## Usage
+  ${data.usage}
+  ## License
+  This license project is covered under the ${data.license} license
+  ## Contributions
+  ${data.contributions}
+  ## Testing
+  ${data.testing}
+  ## Questions
+
+  Email: ${data.email}
+
+  GitHub: github.com/${data.github}
+  `;
+  }
 
 module.exports = generateMarkdown;
+
+// misc comments
 
 // dynamically fill out the text and/or color
 
